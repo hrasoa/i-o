18b23c3efee5969de25e18fd3ac135e3a9e74cd0
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-03-03 05:25:40 +0100

    Initial commit

--- webpack.build.js
+++ webpack.build.js
@@ -2,11 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './dist'
-  },
+  mode: 'production',
+  entry: './src/io.js',
   module: {
     rules: [
       {
@@ -18,14 +15,10 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-      template: 'public/index.html'
-    }),
-  ],
   output: {
     filename: 'io.js',
+    library: 'io',
+    libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist')
   }
 };
