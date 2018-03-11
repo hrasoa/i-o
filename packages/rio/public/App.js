de345e40be77db144814c5b6bd9a79e5db342ded
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-03-11 17:27:48 +0100

    init rio

--- packages/rio/public/App.js
+++ packages/rio/public/App.js
@@ -5,10 +5,20 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [
-        { id: 0, src: 'https://picsum.photos/400/250?image=0' },
-      ],
+      images: [],
     };
+  }
+
+  addImages() {
+    const amount = 10;
+    const images = [];
+    const count = this.state.images.length;
+    for (let i = 0; i < amount; i += 1) {
+      images.push({ id: count + i, src: `https://picsum.photos/400/250?image=${count + i}` });
+    }
+    this.setState(prevState => ({
+      images: [...prevState.images, ...images],
+    }));
   }
 
   render() {
