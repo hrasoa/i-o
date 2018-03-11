8b13d967e32816b46229f10b52b4bcdc8c82ac95
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-03-11 01:32:58 +0100

    Update jsdoc, build

--- packages/io/webpack/build.js
+++ packages/io/webpack/build.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
 module.exports = {
   mode: 'production',
   entry: {
     io: './src/index.js',
     'io.polyfill': './src/polyfill.js',
+  },
+  stats: {
+    colors: true,
+    modules: false,
   },
   module: {
     rules: [
