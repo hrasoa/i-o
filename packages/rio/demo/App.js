720f9da5e23815c2b209ebf9bf1111aad4a31b99
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-03-13 20:27:13 +0100

    update

--- packages/rio/demo/App.js
+++ packages/rio/demo/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import './styles.css';
+import '@hrasoa/io/demo/styles.css';
 
 class App extends Component {
   constructor(props) {
